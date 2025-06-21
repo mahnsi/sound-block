30095fbb5017c122996a87929b4e65be73b9000d
Mahnsi <mahnsir22@gmail.com>
2025-06-21 14:48:22 -0400

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Sound Block
 
+## Pitch
 Nyquist theorem
 
+## Loudness
+can be complex but theres a typical algorithm for calculating loudness with
+RMS
+
+## Timbre
+"colour" of the sound
+
+## Next
 Stereo mode (L R)
+
+v2: transitioning sounds
--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,10 @@
 import librosa as lr
-#path = input("path to the audio file: ")
-path = "MulberryMouse.mp3"
+
+# n_fft is the number of samples in each FFT window. so the size of the window.
+n_fft = 1024
+# hop_length is how many samples the window moves forward in time for each step
+hop_length = 256 
+# the above combination is good for rapid updates and fine for no speech
 
 def preprocess_audio(path):
     # waveform is the audio signal as a numpy array (1D for now)
@@ -21,23 +25,23 @@ def estimate_pitch(waveform, sr):
     fmin = lr.note_to_hz('C2')  
     fmax = lr.note_to_hz('C7')
 
-    # n_fft is the number of samples in each FFT window. so the size of the window.
-    n_fft = 1024
-    # hop_length is how many samples the window moves forward in time for each step
-    hop_length = 256 
-    # the above combination is good for rapid updates and fine for no speech
-
     # YIN is a pitch estimation algorithm
     # f0 (fundamental frequency) will contain the detected pitc in Hz for each time frame a numpy array of size len(waveform) / hop_length
-    f0 = lr.yin(waveform, fmin=fmin, fmax=fmax, sr=sr, hop_length=hop_length)
+    f0 = lr.yin(waveform, fmin=fmin, fmax=fmax, sr=sr, frame_length=n_fft, hop_length=hop_length)
     return f0
 
-def loudness():
+def loudness(waveform):
     # loudness is the perceived volume of the audio signal
     # typically rms (root mean square) is used to measure loudness
     rms = lr.feature.rms(y=waveform, frame_length=n_fft, hop_length=hop_length)
 
+def timbre():
+    spectral_centroid = []
+    return spectral_centroid
+
 def main():
+    #path = input("path to the audio file: ")
+    path = "MulberryMouse.mp3"
     waveform, sr = preprocess_audio(path)
     f0 = estimate_pitch(waveform, sr)
     print(f"Estimated pitch (f0): {f0}")
