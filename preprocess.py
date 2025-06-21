8cb4e1c09e18399e2fd1cd1546de19a541ef0feb
Mahnsi <mahnsir22@gmail.com>
2025-06-21 14:16:16 -0400

    update gitignore, preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,20 @@
 import librosa as lr
-path = input("path to the audio file: ")
+#path = input("path to the audio file: ")
+path = "MulberryMouse.mp3"
 
 def preprocess_audio(path):
     # waveform is the audio signal as a numpy array (1D for now)
     # sr is the sampling rate which tells you how many times per second the audio wave is measured 
-    waveform, sr = lr.load(path, sr=44100, mono=True)
-    return waveform, sr
+    try:
+        print(f"Loading audio file from: {path}")
+        waveform, sr = lr.load(path, sr=44100, mono=True)
+        return waveform, sr
+    except Exception as e:
+        print(f"Error loading audio file: {e}")
+        return None, None
 
 def estimate_pitch(waveform, sr):
+    print("Estimating pitch...")
     # pitch is the frequency of the audio signal in Hz
 
     # we are interested in the pitch range of a piano, which is from C2 to C7
@@ -21,5 +28,19 @@ def estimate_pitch(waveform, sr):
     # the above combination is good for rapid updates and fine for no speech
 
     # YIN is a pitch estimation algorithm
-    # f0 will contain the detected pitch in Hz for each time frame a numpy array of size len(waveform) / hop_length
-    f0 = lr.yin(waveform, fmin=fmin, fmax=fmax, sr=sr, hop_length=512)
+    # f0 (fundamental frequency) will contain the detected pitc in Hz for each time frame a numpy array of size len(waveform) / hop_length
+    f0 = lr.yin(waveform, fmin=fmin, fmax=fmax, sr=sr, hop_length=hop_length)
+    return f0
+
+def loudness():
+    # loudness is the perceived volume of the audio signal
+    # typically rms (root mean square) is used to measure loudness
+    rms = lr.feature.rms(y=waveform, frame_length=n_fft, hop_length=hop_length)
+
+def main():
+    waveform, sr = preprocess_audio(path)
+    f0 = estimate_pitch(waveform, sr)
+    print(f"Estimated pitch (f0): {f0}")
+
+if __name__ == "__main__":
+    main()
